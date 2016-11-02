6abc7463bb10bafe7b1cf2ce47e3d9aecffc16d3
mpollet0192 <mpollet0192@001LAB153010819.neisd.ad>
2016-11-02 13:46:17 -0500

    First commit, assignment complete

---
+++ MathLab2/src/MathLab02st.java
@@ -0,0 +1,119 @@
+// MathLab02st.java
+// The Rational Class Program I
+// This is the student, starting version of the MathLab02 assignment.
+
+
+import javax.swing.JOptionPane;
+
+
+public class MathLab02st
+{
+	public static void main (String args[])
+	{   
+		String strNbr1 = JOptionPane.showInputDialog("Enter Numerator "); 
+		String strNbr2 = JOptionPane.showInputDialog("Enter Denominator ");
+
+		int num = Integer.parseInt(strNbr1);
+		int den = Integer.parseInt(strNbr2);
+
+		Rational r = new Rational(num,den);
+		JOptionPane.showMessageDialog(null,r.getOrigional() +" equals "+r.getDecimal() +  " " + "and reduces to" + " " + r.getRational());
+          
+		System.exit(0);
+	}
+}
+
+				
+
+class Rational
+{
+		private int num, den, firstNum, firstDen;
+		
+//	Rational
+	public Rational (int n, int d)
+	{
+		num = firstNum = n;
+		den = firstDen = d;
+		reduce(); 
+	}
+
+//	getNum
+	public int getNum()
+	{
+		return num;
+	}
+ 
+//	getDen
+	public int getDen()
+	{
+		return den;
+	}
+
+//	getDecimal
+	public double getDecimal()
+	{
+		return (double)num / den;
+		
+	}
+				
+			
+//	getRational 
+	public String getRational()
+
+	{
+		 return num +"/"+ den;
+				
+	}
+
+	
+
+//	getOriginal
+	public String getOrigional()
+
+	{
+		 return firstNum +"/"+ firstDen;
+				
+	}
+
+	public int getOriginalnum()
+	{
+		return firstNum;
+	}
+	
+	public int getOriginalden()
+	{
+		return firstDen;
+	}
+	
+//	reduce
+	public void reduce()
+	{
+		int gcf = getGCF ( firstNum, firstDen);
+		num = firstNum / gcf;
+		den = firstDen / gcf;
+	}
+	
+	
+//	return Math.floor(getDecimal);	
+	
+	
+	private int getGCF (int n1,int n2)
+	{
+		int rem = 0;
+		int gcf = 0;
+		do
+		{
+			rem = n1 % n2;
+			if (rem == 0)
+				gcf = n2;
+			else
+			{
+				n1 = n2;
+				n2 = rem;
+			}
+		}
+		while (rem != 0);
+		return gcf;
+	} 
+}
+
